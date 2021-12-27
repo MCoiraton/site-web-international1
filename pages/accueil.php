a03b8d4af03c5fba9c506ab7a200e8d0850734c4
NIFr3d <92800678+NIFr3d@users.noreply.github.com>
2021-12-27 12:02:40 +0100

    Update accueil.php

--- pages/accueil.php
+++ pages/accueil.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Acceuil</title>
+    <title>Accueil</title>
     <link rel ="stylesheet" href ="../styles/accueil.css">
     <link rel="icon" sizes="180x180" href="images/fav.png">
     <?php
