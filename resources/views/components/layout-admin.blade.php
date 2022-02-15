d5d02a609204c33bab647c22ba591441e168abff
ayoub-echChamali <ayoub2001@hotmail.it>
2022-02-15 12:22:19 +0100

    tweaks

--- resources/views/components/layout-admin.blade.php
+++ resources/views/components/layout-admin.blade.php
@@ -6,9 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="{{ asset('css/app.css')}}" rel="stylesheet">
 </head>
-<body>
-    <div class="flex items-center justify-center bg-gray-200">
-        <nav class="bg-white border-b border-gray-200 fixed z-30 w-full px-3 py-3 lg:px-5 lg:pl-3 flex items-center justify-start">
+<body class="bg-gray-200">
+    <!-- <div class="flex items-center justify-center bg-gray-200"> -->
+        <!-- <nav class="bg-white border-b border-gray-200 rounded-lg fixed z-30 w-full m-3 px-3 py-3 lg:px-5 lg:pl-3 flex items-center justify-start"> -->
+        <nav class="bg-white border-b border-gray-200 rounded-lg p-4 m-6 lg:px-5 lg:pl-3 flex items-center justify-start">
             <a href="/" class="text-xl font-body flex items-center lg:ml-2.5">
                 <img src="{{ asset('img/logo.png')}}" class="h-10 mr-2" alt="Polytech Logo">
             </a>
@@ -24,67 +25,68 @@
                 </div>
             </form>
         </nav>
-        <div class="flex bg-white pt-16">
-            <aside id="sidebar" class="fixed z-20 h-full top-0 left-0 pt-16 flex lg:flex flex-shrink-0 flex-col w-64" aria-label="Sidebar">
-                <div class="relative flex-1 flex flex-col min-h-0 border-r rounded-lg border-gray-200 bg-white pt-0">
-                    <div class="flex-1 flex flex-col pt-5 pb-4 overflow-y-auto">
-                        <div class="flex-1 px-3 bg-white divide-y space-y-1">
-                            <ul class="space-y-2 pb-2">
-                                <li>
-                                <a href="/admin" id="tableau" class="text-base text-gray-900 font-normal rounded-lg flex items-center p-2 hover:bg-gray-300 group">
-                                    {{ $tableau ?? '' }}
-                                    <svg id="tableau" class="w-6 h-6 text-gray-500 group-hover:text-gray-900 transition duration-75" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path>
-                                        <path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path>
-                                    </svg>
-                                    <span class="ml-3">Tableau de Bord</span>
-                                </a>
-                                </li>
-                                <li>
-                                <a href="admin/gestion/" id="gestion" class="text-base text-gray-900 font-normal rounded-lg hover:bg-gray-100 flex items-center p-2 group ">
-                                    {{ $gestion ?? '' }}
-                                    <svg id="gestion" class="w-6 h-6 text-gray-500 flex-shrink-0 group-hover:text-gray-900 transition duration-75" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"></path>
-                                    </svg>
-                                    <span class="ml-3 flex-1 whitespace-nowrap">Gestion Universités</span>
-                                </a>
-                                </li>
-                                <li>
-                                <a href="" id="messagerie" class="text-base text-gray-900 font-normal rounded-lg hover:bg-gray-100 flex items-center p-2 group ">
-                                    {{ $messagerie ?? '' }}    
-                                    <svg class="w-6 h-6 text-gray-500 flex-shrink-0 group-hover:text-gray-900 transition duration-75" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M8.707 7.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l2-2a1 1 0 00-1.414-1.414L11 7.586V3a1 1 0 10-2 0v4.586l-.293-.293z"></path>
-                                        <path d="M3 5a2 2 0 012-2h1a1 1 0 010 2H5v7h2l1 2h4l1-2h2V5h-1a1 1 0 110-2h1a2 2 0 012 2v10a2 2 0 01-2 2H5a2 2 0 01-2-2V5z"></path>
-                                    </svg>
-                                    <span class="ml-3 flex-1 whitespace-nowrap">Messagerie</span>
-                                </a>
-                                </li>
-                                <li>
-                                <a href="" class="text-base text-gray-900 font-normal rounded-lg hover:bg-gray-100 flex items-center p-2 group ">
-                                    {{ $utilisateurs ?? '' }}
-                                    <svg class="w-6 h-6 text-gray-500 flex-shrink-0 group-hover:text-gray-900 transition duration-75" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                        <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path>
-                                    </svg>
-                                    <span class="ml-3 flex-1 whitespace-nowrap">Utilisateurs</span>
-                                </a>
-                                </li>
-                                <li>
-                                <a href="" class="text-base text-gray-900 font-normal rounded-lg hover:bg-gray-100 flex items-center p-2 group ">
-                                    <svg class="w-6 h-6 text-gray-500 flex-shrink-0 group-hover:text-gray-900 transition duration-75" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                        <path fill-rule="evenodd" d="M5 4a3 3 0 00-3 3v6a3 3 0 003 3h10a3 3 0 003-3V7a3 3 0 00-3-3H5zm-1 9v-1h5v2H5a1 1 0 01-1-1zm7 1h4a1 1 0 001-1v-1h-5v2zm0-4h5V8h-5v2zM9 8H4v2h5V8z" clip-rule="evenodd"></path>
-                                    </svg>
-                                    <span class="ml-3 flex-1 whitespace-nowrap">Fiches Candidatures</span>
-                                </a>
-                                </li>
-                            </ul>
+        <div class="flex flex-row">
+            <div class="bg-white p-4 m-6 rounded-lg ">
+                <!-- <aside id="sidebar" class="fixed z-20 h-full top-0 left-0 pt-16 flex lg:flex flex-shrink-0 flex-col w-64" aria-label="Sidebar"> -->
+                <aside id="sidebar" class="w-64" aria-label="Sidebar">
+                    <div class="relative flex-1 flex flex-col rounded-lg bg-white pt-0">
+                        <div class="flex-1 flex flex-col pt-5 pb-4 overflow-y-auto">
+                            <div class="flex-1 px-3 bg-white divide-y space-y-1">
+                                <ul class="space-y-2 pb-2">
+                                    <li>
+                                    <a href="/admin" id="tableau" class="text-base text-gray-900 font-normal rounded-lg flex items-center p-2 hover:bg-gray-300 group">
+                                        {{ $tableau ?? '' }}
+                                        <svg id="tableau" class="w-6 h-6 text-gray-500 group-hover:text-gray-900 transition duration-75" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path>
+                                            <path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path>
+                                        </svg>
+                                        <span class="ml-3">Tableau de Bord</span>
+                                    </a>
+                                    </li>
+                                    <li>
+                                    <a href="admin/gestion/" id="gestion" class="text-base text-gray-900 font-normal rounded-lg hover:bg-gray-100 flex items-center p-2 group ">
+                                        {{ $gestion ?? '' }}
+                                        <svg id="gestion" class="w-6 h-6 text-gray-500 flex-shrink-0 group-hover:text-gray-900 transition duration-75" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"></path>
+                                        </svg>
+                                        <span class="ml-3 flex-1 whitespace-nowrap">Gestion Universités</span>
+                                    </a>
+                                    </li>
+                                    <li>
+                                    <a href="" id="messagerie" class="text-base text-gray-900 font-normal rounded-lg hover:bg-gray-100 flex items-center p-2 group ">
+                                        {{ $messagerie ?? '' }}    
+                                        <svg class="w-6 h-6 text-gray-500 flex-shrink-0 group-hover:text-gray-900 transition duration-75" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M8.707 7.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l2-2a1 1 0 00-1.414-1.414L11 7.586V3a1 1 0 10-2 0v4.586l-.293-.293z"></path>
+                                            <path d="M3 5a2 2 0 012-2h1a1 1 0 010 2H5v7h2l1 2h4l1-2h2V5h-1a1 1 0 110-2h1a2 2 0 012 2v10a2 2 0 01-2 2H5a2 2 0 01-2-2V5z"></path>
+                                        </svg>
+                                        <span class="ml-3 flex-1 whitespace-nowrap">Messagerie</span>
+                                    </a>
+                                    </li>
+                                    <li>
+                                    <a href="" class="text-base text-gray-900 font-normal rounded-lg hover:bg-gray-100 flex items-center p-2 group ">
+                                        {{ $utilisateurs ?? '' }}
+                                        <svg class="w-6 h-6 text-gray-500 flex-shrink-0 group-hover:text-gray-900 transition duration-75" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                            <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path>
+                                        </svg>
+                                        <span class="ml-3 flex-1 whitespace-nowrap">Utilisateurs</span>
+                                    </a>
+                                    </li>
+                                    <li>
+                                    <a href="" class="text-base text-gray-900 font-normal rounded-lg hover:bg-gray-100 flex items-center p-2 group ">
+                                        <svg class="w-6 h-6 text-gray-500 flex-shrink-0 group-hover:text-gray-900 transition duration-75" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                            <path fill-rule="evenodd" d="M5 4a3 3 0 00-3 3v6a3 3 0 003 3h10a3 3 0 003-3V7a3 3 0 00-3-3H5zm-1 9v-1h5v2H5a1 1 0 01-1-1zm7 1h4a1 1 0 001-1v-1h-5v2zm0-4h5V8h-5v2zM9 8H4v2h5V8z" clip-rule="evenodd"></path>
+                                        </svg>
+                                        <span class="ml-3 whitespace-nowrap">Fiches Candidatures</span>
+                                    </a>
+                                    </li>
+                                </ul>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </aside>
+                </aside>
+            </div>
+            <div class="bg-white grow rounded-lg m-6 p-4">
+                {{ $panel ?? '' }}
+            </div>
         </div>
-        <div class="fixed w-max z-40 top-16 mt-2 left-64">
-            {{ $panel ?? '' }}
-        </div>
-        
-    </div>
 </body>
