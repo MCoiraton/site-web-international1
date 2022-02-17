71051edeb6cbfeee6602f21113f5c46bb33491ee
NIFr3d <fred-dls@orange.fr>
2022-02-17 16:13:15 +0100

    v1

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -54,9 +54,8 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $routeMiddleware = [
-        'cas.auth'  => 'Subfission\Cas\Middleware\CASAuth',
-        'cas.guest' => 'Subfission\Cas\Middleware\RedirectCASAuthenticated',
         'admin' => AdminsOnly::class,
+        'polytech' => \App\Http\Middleware\PolytechOnly::class,
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
--- resources/views/profil.blade.php
+++ resources/views/profil.blade.php
@@ -1,33 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Polytech Nancy International</title>
-    <link rel ="stylesheet" href ="{{ asset('css/app.css')}}">
-    @include("nav")
-</head>
-<body>
-<?php
-echo("Prénom : ");
-echo(session("prenom"));
-echo("<br>Nom : ");
-echo(session("nom"));
-echo("<br>Adresse mail : ");
-echo(session("mail"));
-echo("<br>Identifiant UL : ");
-echo(session("uid"));
-echo("<br>Elève polytech ? ");
-if(session("isPolytech")) echo("Oui");
-else echo("Non");
-echo("<br>Admin ? ");
-if(session("isAdmin")) echo("Oui");
-else echo("Non");
-
-?>
-</body>
-<footer>
-@include("footer")
-</footer>
-</html>
+<x-layout-profil>
+    <x-slot name='tableau'>
+		<style>
+            #tableau {
+                background-color: rgba(229, 231, 235, var(--tw-bg-opacity));
+                --tw-text-opacity: 1;
+                color: rgba(17, 24, 39, var(--tw-text-opacity));
+            };
+        </style>
+	</x-slot>
+    <x-slot name='panel'>
+        
+    </x-slot>
+</x-layout-profil>
