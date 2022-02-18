334b88200f552838dc09df4b345e41cb5d4263cd
NIFr3d <fred-dls@orange.fr>
2022-02-18 23:15:10 +0100

    Update layout-profil.blade.php

--- resources/views/components/layout-profil.blade.php
+++ resources/views/components/layout-profil.blade.php
@@ -8,22 +8,7 @@
     <link href="{{ asset('css/app.css')}}" rel="stylesheet">
 </head>
 <body class="bg-gray-200 h-full">
-    <nav class="bg-white border-b border-gray-200 rounded-lg p-4 m-6 lg:px-5 lg:pl-3 flex items-center justify-start">
-        <a href="/" class="text-xl font-body flex items-center lg:ml-2.5">
-            <img src="{{ asset('img/logo.png')}}" class="h-10 mr-2" alt="Polytech Logo">
-        </a>
-        <form action="#" method="GET" class="lg:block lg:pl-32">
-            <label for="topbar-search" class="sr-only">Search</label>
-            <div class="mt-1 relative lg:w-64">
-                <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                    <svg class="w-5 h-5 text-gray-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                    <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path>
-                    </svg>
-                </div>
-                <input type="text" name="email" id="topbar-search" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-cyan-600 focus:border-cyan-600 block w-full pl-10 p-2.5" placeholder="Search">
-            </div>
-        </form>
-    </nav>
+    @include("nav")
     <div class="flex flex-row h-full">
         <div class="bg-white p-4 m-6 rounded-lg ">
             <aside id="sidebar" class="w-64" aria-label="Sidebar">
