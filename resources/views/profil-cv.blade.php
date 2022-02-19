3f67a5427fe6ece8fe4d6b5b1f1749fd549264a2
NIFr3d <fred-dls@orange.fr>
2022-02-19 14:57:03 +0100

    Update profil-cv.blade.php

--- resources/views/profil-cv.blade.php
+++ resources/views/profil-cv.blade.php
@@ -166,19 +166,11 @@
                 <input class="py-2 px-3 rounded-lg border-2 border-blue-300 mt-1 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent" id="numtel" type="text" placeholder="06XXXXXXXX" />
               </div>
 
-
               <div class="grid grid-cols-1 mt-5 mx-7">
-                <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold mb-1">Téléverser votre photo (format 4:3)</label>
-                  <div class='flex items-center justify-center w-full'>
-                      <label class='flex flex-col border-4 border-dashed w-full h-32 hover:bg-gray-100 hover:border-blue-300 group'>
-                          <div class='flex flex-col items-center justify-center pt-7'>
-                            <svg class="w-10 h-10 text-blue-400 group-hover:text-blue-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
-                            <p class='lowercase text-sm text-gray-400 group-hover:text-blue-600 pt-1 tracking-wider'>Sélectionnez une photo</p>
-                          </div>
-                        <input id="photo" type='file' class="hidden" />
-                      </label>
-                  </div>
+                <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-bold">Téléverser votre photo (format 4:3)</label>
+                <input name="photo" id="photo" accept="image/*" class="py-2 px-3 rounded-lg border-2 border-blue-300 mt-1 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent" type="file">
               </div>
+              
               <div id="education">
                 <div class="grid grid-cols-1 mt-5 mx-7">
                   <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-bold">Scolarité</label>
