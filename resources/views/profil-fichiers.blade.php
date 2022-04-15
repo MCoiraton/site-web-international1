9627474f1cbcb805eb6aa2e6325a97de1cbed399
NIFr3d <fred-dls@orange.fr>
2022-04-15 21:55:48 +0200

    Update profil-fichiers.blade.php

--- resources/views/profil-fichiers.blade.php
+++ resources/views/profil-fichiers.blade.php
@@ -33,31 +33,34 @@
         </script>
         <body>
             <section class="text-gray-600 body-font pt-6">
-                <h1>Fichiers en ligne :</h1>
-                @foreach($fichiers as $fichier)
-                <div>
-                    <h2>{{$fichier->nom}}</h2>
-                    <embed
-                        src="../{{$fichier->url}}"
-                        type="application/pdf"
-                        frameBorder="0"
-                        scrolling="auto"
-                        height="600px"
-                        width="1000px"
-                        id={{$fichier->nom}}
-                        style="display:none"
-                    />
-                    <button type="button" id="bouton_{{$fichier->nom}}" class="items-center hover:bg-blue-700 hover:text-white bg-white text-blue-700 px-3 py-2 rounded-md text-sm font-medium" onclick='afficher_pdf("{{$fichier->nom}}")'>Voir</button>
-                    <button type="button" class="items-center hover:bg-red-700 hover:text-white bg-white text-red-700 px-3 py-2 rounded-md text-sm font-medium">Supprimer</button>
+                <div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                    <h1 class="text-gray-700 text-sm font-bold mb-2">Fichiers en ligne :</h1>
+                    @foreach($fichiers as $fichier)
+                    <div>
+                        <h2 class="text-gray-600 text-sm font-semibold mb-2">{{$fichier->nom}}</h2>
+                        <button type="button" id="bouton_{{$fichier->nom}}" class="items-center hover:bg-blue-700 hover:text-white bg-white text-blue-700 px-3 py-2 rounded-md text-sm font-medium" onclick='afficher_pdf("{{$fichier->nom}}")'>Voir</button>
+                        <button type="button" class="items-center hover:bg-red-700 hover:text-white bg-white text-red-700 px-3 py-2 rounded-md text-sm font-medium">Supprimer</button>
+                        <embed
+                            src="../{{$fichier->url}}"
+                            type="application/pdf"
+                            frameBorder="0"
+                            scrolling="auto"
+                            height="600px"
+                            width="1000px"
+                            id={{$fichier->nom}}
+                            style="display:none"
+                        />
+                    </div>
+                    @endforeach
+                    <form enctype="multipart/form-data" class="mt-5" action="{{ route('fichier.store') }}" method="POST">
+                        @csrf
+                        <h2 class="text-gray-700 text-sm font-bold mb-2">Ajouter un fichier :</h2>
+                        <label class="block text-gray-600 text-sm font-semibold mb-2" for="nom"> Nom du fichier (pas besoin de mettre votre nom) :</label>
+                        <input type="text" name="nom" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"/>
+                        <input type="file" name="fichier" accept=".pdf"/>
+                        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">Envoyer</button>
+                    </form>
                 </div>
-                @endforeach
-                <form enctype="multipart/form-data" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" action="{{ route('fichier.store') }}" method="POST">
-                    @csrf
-                    <label class="block text-gray-700 text-sm font-bold mb-2" for="nom"> Nom du fichier (pas besoin de mettre votre nom) </label>
-                    <input type="text" name="nom" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"/>
-                    <input type="file" name="fichier" accept=".pdf"/>
-                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">Envoyer</button>
-                </form>
             </section>
         </body>
     </x-slot>
