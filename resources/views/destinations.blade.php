eae37d85866403623cc1305be047fee496ac6870
NIFr3d <fred-dls@orange.fr>
2022-05-04 09:16:30 +0200

    Update destinations.blade.php

--- resources/views/destinations.blade.php
+++ resources/views/destinations.blade.php
@@ -21,12 +21,14 @@
             <div class="flex flex-wrap ">
                 @foreach($destinations as $destination)
                 <div class="xl:w-1/4 md:w-1/2 p-4">
-                    <div class="bg-gray-100 p-6 rounded-lg">
-                    <img class="h-40 rounded w-full object-cover object-center mb-6" src="{{$photos[$destination->nom]->url}}" alt="content">
-                    <h3 class="tracking-widest text-indigo-500 text-xs font-medium title-font">{{$destination->pays}}, {{$destination->continent}}</h3>
-                    <a href="/destination/{{$destination->nom}}" class="text-lg text-gray-900 font-medium title-font mb-4">{{$destination->nom}}</h2>
-                    <p class="leading-relaxed text-base truncate">{{$destination->intro}}</p>
-                    </div>
+                    <a href="/destination/{{$destination->nom}}">
+                        <div class="bg-gray-100 p-6 rounded-lg">
+                        <img class="h-40 rounded w-full object-cover object-center mb-6" src="{{$photos[$destination->nom]->url}}" alt="content">
+                        <h3 class="tracking-widest text-indigo-500 text-xs font-medium title-font">{{$destination->pays}}, {{$destination->continent}}</h3>
+                        <h2 class="text-lg text-gray-900 font-medium title-font mb-4">{{$destination->nom}}</h2>
+                        <p class="leading-relaxed text-base truncate">{{$destination->intro}}</p>
+                        </div>
+                    </a>
                 </div>
                 @endforeach
             </div>
