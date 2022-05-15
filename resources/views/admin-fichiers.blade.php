1872b4f3a773dfa7a60dae3881db402e8003dc96
NIFr3d <fred-dls@orange.fr>
2022-05-15 12:28:58 +0200

    searchbar

--- resources/views/admin-fichiers.blade.php
+++ resources/views/admin-fichiers.blade.php
@@ -49,36 +49,50 @@
                 };
                 xhr.send();
             }
-            
+            function search(){
+                //doit masquer les fichiers qui ne correspondent pas à la recherche
+                var search = document.getElementById("search").value;
+                var fichiers = document.getElementsByClassName("fichier");
+                for(var i=0; i<fichiers.length; i++){
+                    if(fichiers[i].innerHTML.toLowerCase().indexOf(search.toLowerCase())==-1){
+                        fichiers[i].style.display = "none";
+                    }
+                    else{
+                        fichiers[i].style.display = "block";
+                    }
+                }
+
+            }
         </script>
     <div class="bg-white shadow rounded-lg p-4 sm:p-6 xl:p-8 ">
+        <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" type="text" placeholder="Rechercher" id="search" onkeyup="search()">
         @foreach($fichiers as $byuid)
-        <div>
+        <div class="fichier" class="mt-4">
             {{$byuid[0]->nomprenom}} ({{$byuid[0]->uid}})
             <button type="button" id="bouton_{{$byuid[0]->uid}}" class="items-center hover:bg-blue-700 hover:text-white bg-white text-blue-700 px-3 py-2 rounded-md text-sm font-medium" onclick='afficher_detail("{{$byuid[0]->uid}}")'>v</button>
             <div id="{{$byuid[0]->uid}}" style="display:none">
             @foreach($byuid as $fichier)
-            <div>
-                <h2 class="text-gray-600 text-sm font-semibold mb-2">{{$fichier->nom}}</h2>
-                <button type="button" id="bouton_{{$fichier->nom}}" class="items-center hover:bg-blue-700 hover:text-white bg-white text-blue-700 px-3 py-2 rounded-md text-sm font-medium" onclick='afficher_pdf("{{$fichier->nom}}")'>Voir</button>
-                <button type="button" id="bouton_{{$fichier->nom}}" class="items-center hover:bg-blue-700 hover:text-white bg-white text-blue-700 px-3 py-2 rounded-md text-sm font-medium" onclick='telecharger_pdf("{{$fichier->url}}","{{$fichier->nom}}")'>Télécharger</button>
-                <form action="{{route('fichier.delete')}}" class="inline" method="POST">
-                    @csrf
-                    <input type="hidden" name="id" value="{{$fichier->id}}">
-                    <input type="hidden" name="redirect" value="/admin/fichiers">
-                    <button type="submit" class="items-center hover:bg-red-700 hover:text-white bg-white text-red-700 px-3 py-2 rounded-md text-sm font-medium">Supprimer</button>
-                </form>
-                <embed
-                    src="/storage/{{$fichier->url}}"
-                    type="application/pdf"
-                    frameBorder="0"
-                    scrolling="auto"
-                    height="600px"
-                    width="1000px"
-                    id={{$fichier->nom}}
-                    style="display:none"
-                />
-            </div>
+                <div>
+                    <h2 class="text-gray-600 text-sm font-semibold mb-2">{{$fichier->nom}}</h2>
+                    <button type="button" id="bouton_{{$fichier->nom}}" class="items-center hover:bg-blue-700 hover:text-white bg-white text-blue-700 px-3 py-2 rounded-md text-sm font-medium" onclick='afficher_pdf("{{$fichier->nom}}")'>Voir</button>
+                    <button type="button" id="bouton_{{$fichier->nom}}" class="items-center hover:bg-blue-700 hover:text-white bg-white text-blue-700 px-3 py-2 rounded-md text-sm font-medium" onclick='telecharger_pdf("{{$fichier->url}}","{{$fichier->nom}}")'>Télécharger</button>
+                    <form action="{{route('fichier.delete')}}" class="inline" method="POST">
+                        @csrf
+                        <input type="hidden" name="id" value="{{$fichier->id}}">
+                        <input type="hidden" name="redirect" value="/admin/fichiers">
+                        <button type="submit" class="items-center hover:bg-red-700 hover:text-white bg-white text-red-700 px-3 py-2 rounded-md text-sm font-medium">Supprimer</button>
+                    </form>
+                    <embed
+                        src="/storage/{{$fichier->url}}"
+                        type="application/pdf"
+                        frameBorder="0"
+                        scrolling="auto"
+                        height="600px"
+                        width="1000px"
+                        id={{$fichier->nom}}
+                        style="display:none"
+                    />
+                </div>
             @endforeach
             </div>
         </div>
