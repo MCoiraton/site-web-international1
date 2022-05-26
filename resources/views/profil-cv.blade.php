05ff633bb61b509b9288d4d710274d9fd0d48fe2
NIFr3d <fred-dls@orange.fr>
2022-05-26 11:56:44 +0200

    fix css 2

--- resources/views/profil-cv.blade.php
+++ resources/views/profil-cv.blade.php
@@ -1,7 +1,7 @@
 <x-layout-profil>
-  <x-slot name='tableau'>
+  <x-slot name='cv'>
   <style>
-          #gestion {
+          #cv {
               background-color: rgba(229, 231, 235, var(--tw-bg-opacity));
               --tw-text-opacity: 1;
               color: rgba(17, 24, 39, var(--tw-text-opacity));
